87c741a2a10825ffce9d08e6aa8411f0e1ad7cd0
Mobbynet <118823684+Mobbynet@users.noreply.github.com>
2025-06-09 21:15:19 +0200

    Added edytuj połączenia do panel admina

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ import  random
 
 ###########
 #TODO:
+#    - podzielić na pałe plików bo kongo sie robi w tym pliku
+#    -add edytuj użytkownika
 #    -add all admin powers to panel admina
 #    -add some admin powers to panel przewoznika
 ###########
@@ -222,9 +224,10 @@ def logout():
     return redirect(url_for('login'))
 
 
-#   Wypadałoby to przenieśc do innych plików
-#       |
-#      \/
+######
+#ADMIN
+######
+
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
@@ -279,6 +282,97 @@ def edytuj_bilet(user_id, ticket_id):
     cursor.close()
 
     return redirect(url_for('pokaz_bilety_pasazera', user_id=user_id))
+
+
+@app.route('/admin/polaczenia')
+def admin_polaczenia():
+    cursor = mysql.connection.cursor()
+    cursor.execute("""
+    SELECT 
+        p.id_połączenia,
+        l.nazwa_linii AS nazwa_linii,
+        sp.nazwa_stacji AS stacja_początkowa,
+        sk.nazwa_stacji AS stacja_końcowa,
+        p.id_pociągu,
+        po.model_pociągu AS model,
+        pr.nazwa as przewoznik,
+        p.czas_przejazdu,
+        p.data,
+        p.opóźnienie
+    FROM polaczenia p
+    JOIN linie_kolejowe l ON p.id_lini = l.id_linii
+    JOIN stacje_kolejowe sp ON p.id_stacji_początkowej = sp.id_stacji
+    JOIN stacje_kolejowe sk ON p.id_stacji_końcowej = sk.id_stacji
+    JOIN pociagi po ON p.id_pociągu = po.id_pociągu
+    JOIN przewoznicy pr ON po.id_przewoźnika = pr.id_przewoznika;
+    """)
+    connections = cursor.fetchall()
+
+    cursor.close()
+    return render_template('admin/polaczenia.html', connections=connections)
+
+
+@app.route('/admin/polaczenia/<int:connection_id>/edytuj', methods=['GET', 'POST'])
+def edytuj_polaczenie(connection_id):
+    cursor = mysql.connection.cursor()
+
+    if request.method == 'POST':
+        cursor.execute("""
+            SELECT id_lini FROM polaczenia WHERE id_połączenia = %s
+        """, (connection_id,))
+        
+
+        id_linii = request.form.get('id_linii')
+        id_stacji_poczatkowej = request.form.get('id_stacji_początkowej')
+        id_stacji_koncowej = request.form.get('id_stacji_końcowej')
+        id_pociagu = request.form.get('id_pociągu')
+        czas_przejazdu = request.form.get('czas_przejazdu')
+        data = request.form.get('data')
+        opoznienie = request.form.get('opóźnienie')
+
+        cursor.execute("""
+            UPDATE polaczenia SET
+                id_lini = %s,
+                id_stacji_początkowej = %s,
+                id_stacji_końcowej = %s,
+                id_pociągu = %s,
+                czas_przejazdu = %s,
+                data = %s,
+                opóźnienie = %s
+            WHERE id_połączenia = %s
+        """, (id_linii, id_stacji_poczatkowej, id_stacji_koncowej, id_pociagu, czas_przejazdu, data, opoznienie, connection_id))
+        
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for('admin_polaczenia'))
+
+    cursor.execute("""
+        SELECT id_połączenia, id_lini, id_stacji_początkowej, id_stacji_końcowej, id_pociągu, czas_przejazdu, data, opóźnienie 
+        FROM polaczenia WHERE id_połączenia = %s
+    """, (connection_id,))
+    connection = cursor.fetchone()
+
+
+    cursor.execute("SELECT id_stacji, nazwa_stacji FROM stacje_kolejowe ORDER BY nazwa_stacji")
+    stacje = cursor.fetchall()
+
+
+    cursor.execute("SELECT id_pociągu FROM pociagi ORDER BY id_pociągu")
+    pociagi = cursor.fetchall()
+
+
+    cursor.execute("SELECT id_linii, nazwa_linii FROM linie_kolejowe ORDER BY nazwa_linii")
+    linie = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('admin/edytuj_polaczenie.html',
+                           connection=connection,
+                           stacje=stacje,
+                           pociagi=pociagi,
+                           linie=linie)
+
+
 
 
 if __name__ == '__main__':
